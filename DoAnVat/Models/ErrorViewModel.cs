18e40b1871d70a24ab7cf61ef4ee6daaf7e55b1e
TruongVanTan1212 <truongtan260518@gmail.com>
2023-12-14 21:10:24 +0700

    project

---
+++ DoAnVat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoAnVat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
